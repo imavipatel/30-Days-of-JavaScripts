5e8e8c305936a1e9ec8fdcd1228d37f730bdcc6e
Ankit Patel <ankit.patel4@aubank.in>
2025-03-18 16:17:58 +0530

    First commit

---
+++ day1.js
@@ -0,0 +1,40 @@
+console.log('this is in external file of introduction js')
+
+let firstName
+
+var secondName
+
+console.log(firstName)
+console.log(secondName)
+// we need to assign const we cant let it empty.
+
+
+
+// Introduction to data types
+// Number, String, boolean, null, undefined, Symbol
+
+// Number = > Integers => -1,-2,-3,-4, 0,1,2,3,4
+//  Number => Float Point numbers => 1.2,3.3,4.5, -5.4
+
+
+
+const myName = null
+console.log(myName)
+
+
+console.log(typeof  null) //object
+console.log(typeof Symbol) //function
+console.log(typeof undefined) //undefined
+
+//type of comments
+
+//single line of comment
+
+/* multiple line of comment
+ this is multiple line of comment */
+
+ //Variable
+
+ //A javascript variable does not allow special character except $ and _ 
+
+ 
