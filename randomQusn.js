28a0c4751bb41332c5b4304d7468c35a59115197
Ankit Patel <ankit.patel4@aubank.in>
2025-05-30 11:57:18 +0530

    Added random qusn

---
+++ randomQusn.js
@@ -0,0 +1,31 @@
+//Random qurated question
+
+let x = 10;
+
+const check = ()=>{
+    console.log(x);
+    if(true){
+        var x = 20
+    }
+}
+
+check()
+
+console.log(1+'1');
+console.log('11'-1)
+
+
+const arr = [1, , 3]
+
+console.log(arr.length)
+console.log(arr[1])
+console.log(1 in arr)
+console.log(2 in arr)
+
+
+let first = 'Ankit'
+let second = '❤️'
+
+console.log(first.length)
+console.log(second.length) //js use UTF-16 under the hood. js counts units not actual visible char
+
